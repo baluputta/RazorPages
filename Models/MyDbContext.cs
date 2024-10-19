5ba4c6e41010aee3dc163441155b95bd5a787fae
balu <168086202+baluputta@users.noreply.github.com>
2024-10-19 15:00:22 +0530

    Add files via upload

---
+++ Models/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RazorPages.Models
+{
+    public class MyDbContext : DbContext
+    {
+
+        public DbSet<Person> People { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MyDb");
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace RazorPages.Models
+{
+    public class Person
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public int Age { get; set; }
+    }
+}
